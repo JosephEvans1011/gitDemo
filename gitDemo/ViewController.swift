708dc5c3ee015d7d3233a187fdde6b768124c2ba
Joseph <47125088+JosephEvans1011@users.noreply.github.com>
2019-04-01 09:32:56 +0100

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Joseph Evans on 01/04/2019.
+//  Copyright © 2019 Joseph Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
